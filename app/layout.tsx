e3f34684cc3c74ad7bc2ea149e095500d90e05b0
markabaever-debug <markabaever@gmail.com>
2026-02-27 11:18:54 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXXX');
+              })(window,document,'script','dataLayer','GTM-MNCSPZM6');
             `,
           }}
         />
@@ -50,7 +50,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MNCSPZM6"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
