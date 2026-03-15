51fcc6bc14cf01177cdd268421dbf30418eb41b7
markabaever-debug <markabaever@gmail.com>
2026-03-15 13:17:41 +0500

    бновления сайта адвоката

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
+import { FloatingContactButtons } from "@/components/FloatingContactButtons";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -58,6 +59,7 @@ export default function RootLayout({
         </noscript>
 
         {children}
+        <FloatingContactButtons />
       </body>
     </html>
   );
